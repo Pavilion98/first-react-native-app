65198d3013e108f68df351dca950d930f13421e7
wassim <wassim.alharakeh@mubs.edu.lb>
2021-09-23 19:50:39 +0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+
+const TravelWebsite = 'https://pavilion98.github.io/'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style = {{ width: '100%', height: '100%'}}> 
+        <WebView 
+          source= { { uri: TravelWebsite} }
+          onLoad = {console.log('Loaded!')}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,6 +22,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop : 28,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
